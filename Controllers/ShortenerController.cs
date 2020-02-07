8a5631dcc3fb922bab3ede0d667c58eeb233116a
behrad khorram <behrrad@aut.ac.ir>
2020-02-07 21:58:29 +0330

    Post methods added.

--- Controllers/ShortenerController.cs
+++ Controllers/ShortenerController.cs
@@ -5,10 +5,33 @@ using urlShortener.Models;
 using System.Text.RegularExpressions;
 using System;    
 using System.Text; 
+using urlShortener.Services;
 namespace urlShortener.Controllers
 {
     [ApiController]
     [Route("/urls")]    
     public class ShortenerController : ControllerBase{
+        public ShortenerService shortener;
+        public ShortenerController(ShortenerService shortener){
+            this.shortener = shortener;
+        }
+
+        private bool ValidateUrl(string url){
+            url = url.Trim();
+            Regex pattern = new Regex(@"^(?:((http(s)|ftp)://))?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$");
+            Match match = pattern.Match(url);
+            if (match.Success == false) {
+                return false;
+            }
+            return true;
+        }
+        [HttpPost]
+        public ActionResult<string> shortner([FromBody]SingleUrl longUrl){
+            if(!ValidateUrl(longUrl.url)){
+                return BadRequest("in url validate nist");
+            }
+            
+            return Ok(shortener.setShortUrl(longUrl));
+        }
     }
 }
--- Services/ShortenerService.cs
+++ Services/ShortenerService.cs
@@ -9,6 +9,10 @@ using System.Threading.Tasks;
 
 namespace urlShortener.Services
 {
+    static class Constants{
+        public static int size = 8;
+    }
+    
     public class ShortenerService
     {
         public AppDbContext dbContext { get; set; }
@@ -16,5 +20,44 @@ namespace urlShortener.Services
         public ShortenerService (AppDbContext dbContext) { 
             this.dbContext = dbContext;
         }
+        private string RandomString()  
+        {  
+            StringBuilder builder = new StringBuilder();  
+            Random random = new Random();  
+            char ch;  
+            int isLowerCase = 0;
+            for (int i = 0; i < Constants.size; i++)  
+            {  
+                isLowerCase = Convert.ToInt32(Math.Floor(2 * random.NextDouble()));
+                if(isLowerCase == 1){
+                    isLowerCase = 32;
+                }
+                else{
+                    isLowerCase = 0;
+                }
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65 + isLowerCase)));  
+                builder.Append(ch);  
+            } 
+            return builder.ToString();  
+        } 
+
+        public string setShortUrl(SingleUrl longUrl)
+        {
+            string shortUrl ;
+            Task<bool> search;
+            do{
+                shortUrl = RandomString();
+                search = dbContext.Urls.AnyAsync(p => p.shortUrl == shortUrl);
+                search.Wait();
+            }while(search.Result);
+
+
+            Url newUrl = new Url(longUrl.url, shortUrl);
+
+            dbContext.Urls.Add(newUrl); 
+            dbContext.SaveChanges();   
+            return shortUrl;
+        }
+
     }
 }
